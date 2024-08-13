6c48471536abf21042cde108354447b96327fadf
Memoryman2022 <rlacannon@gmail.com>
2024-08-13 23:55:42 +0200

    TEST

--- vite.config.js
+++ vite.config.js
@@ -1,3 +1,25 @@
+// import { defineConfig } from "vite";
+// import react from "@vitejs/plugin-react";
+
+// // https://vitejs.dev/config/
+// export default defineConfig({
+//   plugins: [react()],
+//   resolve: {
+//     alias: {
+//       "react-router-dom": "react-router-dom",
+//     },
+//   },
+//   server: {
+//     proxy: {
+//       "/api": {
+//         target: "https://api.football-data.org",
+//         changeOrigin: true,
+//         rewrite: (path) => path.replace(/^\/api/, ""),
+//       },
+//     },
+//   },
+// });
+
 import { defineConfig } from "vite";
 import react from "@vitejs/plugin-react";
 
@@ -6,7 +28,9 @@ export default defineConfig({
   plugins: [react()],
   resolve: {
     alias: {
-      "react-router-dom": "react-router-dom",
+      // You can add aliases here if needed
+      // For example, aliasing some paths
+      // '@components': '/src/components',
     },
   },
   server: {
