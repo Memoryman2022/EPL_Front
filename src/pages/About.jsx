8456c980ac1aa076460746fbb67e0383100f1200
Memoryman2022 <rlacannon@gmail.com>
2024-08-15 14:30:37 +0200

    about

--- src/pages/About.jsx
+++ src/pages/About.jsx
@@ -1,18 +1,25 @@
 import React from "react";
+import { Link } from "react-router-dom";
 import "../css/About.css"; // Make sure you have a CSS file for styling
 
 const About = () => {
   return (
     <div className="about-container">
-      <h1>About The Premier League Predictor Module</h1>
+      <h3>THE PREMIER LEAGUE PREDICTOR MODULE</h3>
       <p>
-        Welcome to <strong>The Premier League Predictor Module</strong>, your
-        exclusive platform for predicting Premier League matches! Whether you're
-        a casual fan or part of a social group, this app provides a fun and
-        engaging way to test your football knowledge and compete with friends.
+        Welcome to{" "}
+        <strong className="highlighted-text">
+          <Link to="/base" className="about-link">
+            THE PREMIER LEAGUE PREDICTOR MODULE
+          </Link>
+        </strong>
+        , your exclusive platform for predicting Premier League matches! Whether
+        you're a casual fan or part of a social group, this app provides a fun
+        and engaging way to test your football knowledge and compete with
+        friends.
       </p>
 
-      <h2>Our Mission</h2>
+      <h5>MISSION</h5>
       <p>
         Our mission is to showcase the stylings and functionality of modern web
         development through a polished, user-friendly application. Developed by
@@ -20,7 +27,7 @@ const About = () => {
         portfolio and dedication to crafting high-quality software.
       </p>
 
-      <h2>Exclusive Features</h2>
+      <h5>FEATURES</h5>
       <ul>
         <li>Predict match outcomes and scores with ease.</li>
         <li>
@@ -31,20 +38,27 @@ const About = () => {
         <li>Intuitive user interface designed for a smooth experience.</li>
       </ul>
 
-      <h2>Who Should Use This App?</h2>
+      <h5>USERS</h5>
       <p>
-        <strong>The Premier League Predictor Module</strong> is perfect for
-        casual football fans and social groups looking to add a competitive edge
-        to their Premier League experience. Challenge your friends and see who
-        has the sharpest football mind!
+        <strong className="highlighted-text">
+          <Link to="/base" className="about-link">
+            THE PREMIER LEAGUE PREDICTOR MODULE
+          </Link>
+        </strong>{" "}
+        is perfect for casual football fans and social groups looking to add a
+        competitive edge to their Premier League experience. Challenge your
+        friends and see who has the sharpest football mind!
       </p>
 
-      <h2>About the Developer</h2>
+      <h5>DEVELOPER</h5>
       <p>
         I'm an independent coder passionate about building engaging and
         functional web applications. This project is a key part of my portfolio
-        as I work on expanding my skills and showcasing what I can do. Thank you
-        for checking out the app!
+        as I work on expanding my skills and showcasing my abilities. Thank you.
+        Now get out there and start{" "}
+        <strong className="highlighted-text">
+          <Link to="/base"> PREDICTING! </Link>
+        </strong>
       </p>
     </div>
   );
