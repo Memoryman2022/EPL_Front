8118ad389a2d7c03529c5b5cad9050093b281e4b
Memoryman2022 <rlacannon@gmail.com>
2024-08-26 19:00:03 +0200

    pleaase work

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -52,7 +52,7 @@ function Navbar() {
   const handleUpdateResults = async () => {
     try {
       await axios.post(
-        "https://eplbackend.adaptable.app/api/admin/updateResults",
+        "https://eplbackend.adaptable.app/api/updateResults",
         {},
         {
           headers: { Authorization: `Bearer ${token}` },
@@ -68,7 +68,7 @@ function Navbar() {
   const handleUpdateScores = async () => {
     try {
       await axios.post(
-        "https://eplbackend.adaptable.app/api/admin/updateScores",
+        "https://eplbackend.adaptable.app/api/updateScores",
         {},
         {
           headers: { Authorization: `Bearer ${token}` },
