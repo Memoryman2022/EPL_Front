220658b0941307fd192964497771df3e0d517567
Memoryman2022 <rlacannon@gmail.com>
2025-08-12 19:08:16 +0200

    25/26

--- src/components/ResponsiveTeamName.jsx
+++ src/components/ResponsiveTeamName.jsx
@@ -1,4 +1,4 @@
-import React, { useState, useEffect } from "react";
+import { useState, useEffect } from "react";
 import { truncateTeamName } from "../../utils/truncateTeamName";
 
 const ResponsiveTeamName = ({ name }) => {
